f486f19645d85f65e99b64637b968edf7d8536b6
Jay Wagnon <jaywagnon@gmail.com>
2014-11-19 10:48:21 -0700

    Add "no coverage" option.

--- spec/simplecov_helper.rb
+++ spec/simplecov_helper.rb
@@ -1,13 +1,14 @@
-require 'simplecov'
-SimpleCov.minimum_coverage 100
-SimpleCov.start 'rails' do
-end
+unless ENV['NO_COVERAGE']
+  require 'simplecov'
+  SimpleCov.minimum_coverage 100
+  SimpleCov.start
 
-SimpleCov.at_exit do
-  SimpleCov.result.format!
-  if SimpleCov.result.covered_percent < SimpleCov.minimum_coverage
-    fail_msg = "#{'%.2f' % SimpleCov.result.covered_percent}% test coverage."
-    STDERR.puts "\033[0;31m#{fail_msg}\033[0m\nWrite more tests"
-    exit 1
+  SimpleCov.at_exit do
+    SimpleCov.result.format!
+    if SimpleCov.result.covered_percent < SimpleCov.minimum_coverage
+      fail_msg = "#{'%.2f' % SimpleCov.result.covered_percent}% test coverage."
+      STDERR.puts "\033[0;31m#{fail_msg}\033[0m\nWrite more tests"
+      exit 1
+    end
   end
 end
